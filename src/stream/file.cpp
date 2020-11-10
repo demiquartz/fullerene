bfb68e8ed35737b39b6904752bc01aaa11954f34
Takaaki Sato <takaaki@demiquartz.jp>
2020-11-10 23:18:08 +0900

    Add override specifier to file class methods

--- src/stream/file.cpp
+++ src/stream/file.cpp
@@ -16,36 +16,36 @@ public:
     File(const std::string& path, const std::string& mode) : Handle_(std::fopen(path.c_str(), (mode.find('b') == std::string::npos ? mode + 'b' : mode).c_str())) {
     }
 
-    virtual ~File() {
+    virtual ~File() override {
         if (Handle_) std::fclose(Handle_);
     }
 
-    virtual explicit operator bool() const {
+    virtual explicit operator bool() const override {
         return Handle_ != nullptr;
     }
 
-    virtual std::size_t Size(void) {
+    virtual std::size_t Size(void) override {
         struct stat s;
         return fstat(fileno(Handle_), &s) == 0 ? s.st_size : 0;
     }
 
-    virtual std::size_t Read(void* data, std::size_t size) {
+    virtual std::size_t Read(void* data, std::size_t size) override {
         return std::fread(data, 1, size, Handle_);
     }
 
-    virtual std::size_t Write(const void* data, std::size_t size) {
+    virtual std::size_t Write(const void* data, std::size_t size) override {
         return std::fwrite(data, 1, size, Handle_);
     }
 
-    virtual bool Flush(void) {
+    virtual bool Flush(void) override {
         return std::fflush(Handle_) == 0;
     }
 
-    virtual bool Seek(std::size_t offset) {
+    virtual bool Seek(std::size_t offset) override {
         return std::fseek(Handle_, offset, SEEK_SET);
     }
 
-    virtual std::size_t Tell(void) {
+    virtual std::size_t Tell(void) override {
         return std::ftell(Handle_);
     }
 

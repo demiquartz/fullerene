3ce025288a24ffd2b0b5e75cf6750980f2f76750
Takaaki Sato <takaaki@demiquartz.jp>
2020-11-17 21:46:16 +0900

    Add verification to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,30 @@
 #include <string>
 #include <vector>
 #include "stream/file.hpp"
+#include "video/renderer/vulkan.hpp"
 #include "version.hpp"
 
 int main(int argc, char** argv) {
     std::cout << REVISION << std::endl << std::endl;
-    Fullerene::Stream::FileFactory fileFactory;
-    auto file = fileFactory.Open("../LICENSE", "r");
+    auto fileFactory = std::make_shared<Fullerene::Stream::FileFactory>();
+    auto file        = fileFactory->Open("../LICENSE", "r");
     if (!file) return EXIT_FAILURE;
     std::vector<char> text(file->Size());
     if (file->Read(text.data(), text.size()) != text.size()) return EXIT_FAILURE;
     std::cout << std::string_view(text.data(), text.size()) << std::endl;
+    {
+        {
+            Fullerene::Video::Detail::Vulkan renderer;
+            Fullerene::Video::Detail::Vulkan renderer1;
+            Fullerene::Video::Detail::Vulkan renderer2;
+        }
+        Fullerene::Video::Detail::Vulkan renderer3;
+        {
+            Fullerene::Video::Detail::Vulkan renderer4;
+            Fullerene::Video::Detail::Vulkan renderer5;
+            Fullerene::Video::Detail::Vulkan renderer6;
+        }
+        Fullerene::Video::Detail::Vulkan renderer7;
+    }
     return EXIT_SUCCESS;
 }
